d4ffd74f63c71658d6521acacf8c8fa0f4339767
RH <rahim.heydarov@usi.ch>
2022-11-07 03:32:17 +0100

    ..

--- redisCluster_Check.py
+++ redisCluster_Check.py
@@ -1,10 +1,11 @@
 import os
 
+ssh_params = "-i /home/usi/.ssh/controller-root -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 fileName = "redisCluster_Nodes.txt"
 nodes = [line.rstrip('\n') for line in open(fileName)]
 
 for node in nodes:
-    myCmd = "ssh ubuntu@" + node + " 'redis-cli --cluster check " + node + ":6378'"
+    myCmd = "ssh " + ssh_params + " ubuntu@" + node + " 'redis-cli --cluster check " + node + ":6378'"
     os.system(myCmd)
 
     break
