0866df7944978162882a77ff741b3a0f7b81827f
RH <rahim.heydarov@usi.ch>
2022-11-07 03:08:52 +0100

    ..

--- check_redis_service_status.py
+++ check_redis_service_status.py
@@ -1,12 +1,14 @@
 import os
 import time
 
+ssh_params = "-i /home/usi/.ssh/controller-root -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 fileName = "redisCluster_Nodes.txt"
+
 nodes = [line.rstrip('\n') for line in open(fileName)]
 
 # Stop Redis service and delete the cluster configuration and DB on each node
 for node in nodes:
-    myCmd = "ssh ubuntu@" + node + " 'sudo service redis_6378 status'"
+    myCmd = "ssh " + ssh_params + " ubuntu@" + node + " 'sudo service redis_6378 status'"
     os.system(myCmd)
 
 exit(0)
