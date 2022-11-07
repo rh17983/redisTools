8125b1a4ea50f1437829d2f8d924c70a5fad22d7
RH <rahim.heydarov@usi.ch>
2022-11-07 02:36:32 +0100

    ..

--- redisCluster_Build.py
+++ redisCluster_Build.py
@@ -1,12 +1,14 @@
 import os
 
+ssh_params = "-i /home/usi/.ssh/controller-root -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
 fileName = "redisCluster_Nodes.txt"
 nodes = [line.rstrip('\n') for line in open(fileName)]
 
 # Reset Cluster
 for node in nodes:
     # Reset
-    myCmd = "ssh ubuntu@" + node + " 'redis-cli -h " + node + " -p 6378 -c cluster reset'"
+    myCmd = "ssh " + ssh_params + " ubuntu@" + node + " 'redis-cli -h " + node + " -p 6378 -c cluster reset'"
     print(myCmd)
     os.system(myCmd)
 
@@ -20,11 +22,11 @@ for node in nodes:
     nodes_with_ports = nodes_with_ports + node + ":6378"
     i = i + 1
 
-myCmd = "ssh ubuntu@" + nodes[0] + " 'redis-cli --cluster create " + nodes_with_ports + " --cluster-replicas 1'"
+myCmd = "ssh " + ssh_params + " ubuntu@" + nodes[0] + " 'redis-cli --cluster create " + nodes_with_ports + " --cluster-replicas 1'"
 os.system(myCmd)
 
 # Start metricbeats agents on the cluster nodes
-myCmd = "ansible-playbook ~/ansible-beat/playbook.yml -i hosts2 --tags 'start'"
-os.system(myCmd)
+# myCmd = "ansible-playbook ~/ansible-beat/playbook.yml -i hosts2 --tags 'start'"
+# os.system(myCmd)
 
 exit(0)
